835a81fb38a9fe92e89eb1dee5290809bdca48f3
a163236 <a163236>
2022-07-09 22:22:12 +0900

    fix clean.sh

--- clean.sh
+++ clean.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/bash
 
-// targetファイル削除
+# targetファイル削除
 for i in $(find target);do
   rm $i -rf
 done
 
-// firファイル消去
+# firファイル消去
 for i in $(find *.fir);do
     rm $i
 done
 
-// jsonファイル消去
+# jsonファイル消去
 for i in $(find *.json);do
     rm $i
 done
 
-// verilogファイル消去
+# verilogファイル消去
 for i in $(find *.v);do
     rm $i
 done
